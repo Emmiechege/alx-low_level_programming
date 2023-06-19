787ec617936ddfd2726145dd590bf48b279ced85
Emmiechege <emilywanjachege@gmail.com>
2023-06-19 09:19:28 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include<stdio.h>
 /**
- * main - Entry point 
- *
- * Return: Always 0 (success)
+ * main - Entry point
+ *  Return: Always 0 (Success)
  */
-int maid(void)
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
