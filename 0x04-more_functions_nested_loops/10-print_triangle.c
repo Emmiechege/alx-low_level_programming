7d4a011ab6e9650dcf1cd1fbcf83fe509daf7164
Emmiechege <emilywanjachege@gmail.com>
2023-06-23 10:23:57 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,6 +15,7 @@ void print_triangle(int size)
 
 		int i, j;
 
+
 		for (i = 1; i <= size; i++)
 		{
 
@@ -23,7 +24,8 @@ void print_triangle(int size)
 				_putchar(' ');
 			}
 
-			for (j = 1; j <= 1; j++)
+
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
