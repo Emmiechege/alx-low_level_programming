47ed209001d40370c2f4ed3b50aa00379c47156a
Emmiechege <emilywanjachege@gmail.com>
2023-07-30 13:23:53 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integers
+ * @num1: variable one.
+ * @num2: variable two.
+ * Return: result.
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
