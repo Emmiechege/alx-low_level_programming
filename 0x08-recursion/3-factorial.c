1804891fa9f31deb39deb7675505be4bd9fa90c7
Emmiechege <emilywanjachege@gmail.com>
2023-08-02 07:25:00 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,7 +3,7 @@
 /**
  * factorial - a function that returns the factorial of a given number.
  * @n: number to be manipulated.
- * Return: -1(n < 0), 1(n == 0)
+ * Return: -1(n < 0), 1(n == 0), else (factorial of n)
  */
 
 int factorial(int n)
@@ -12,6 +12,13 @@ int factorial(int n)
 	{
 		return (-1);
 	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
 	return (n * factorial(n - 1));
+	}
 }
 
