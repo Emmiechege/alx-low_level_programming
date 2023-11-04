b055e29e57893dbe1c5bd1273e03708d78d575fe
Emmiechege <emilywanjachege@gmail.com>
2023-11-04 13:15:35 -0700

    a program that displays the information contained in the ELF header at the start of an ELF file.

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,26 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magi(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+
+/**
+ * check_elf - checks if a file is an ELF file
+ * @e_ident: Apointer to an array containing ELF magic numbers
+ *
+ * Description: if the file is not an ELF file, exit code 98
+ */
+void check_elf(unsigned char *e_ident)
