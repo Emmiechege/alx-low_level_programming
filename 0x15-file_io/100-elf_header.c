53386405e551e6dd94ad9f5d4ac6834b6bffed70
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 12:19:32 -0800

    A program that displays the information contained in the ELF header at the head of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -84,3 +84,27 @@ void print_class(unsigned char *e_ident)
 			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
+/**
+ * print_data - Prints the data of an ELF header
+ * @e_ident: A pointer to an array containing the ELF class
+ */
+void print_data(unsigned char *e_ident)
+{
+	printf(" Data:					");
+
+	switch (e_ident[EI_DATA])
+	{
+	case ELFDATANONE:
+		printf("none\n");
+		break;
+	case ELFDATA2LSB:
+		printf("2's complement, little endian\n");
+		break;
+	case ELFDATA2MSB:
+		printf("2's complement, big endian\n");
+		break;
+	default:
+		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
+
