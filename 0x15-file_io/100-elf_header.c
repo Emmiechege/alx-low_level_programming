5745c9933a7837670f0683b3a58abdcbd081c160
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 12:07:47 -0800

    A program that displays the information contained in the ELF header at the head of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -61,3 +61,26 @@ void print_magic(unsigned char *e_ident)
 			printf(" ");
 	}
 }
+/**
+ * print_class - Prints the class of an ELF header.
+ * @e_dent: A pointer to an array containing the ELF class
+ */
+void print_class(unsigned char *e_ident)
+{
+	printf(" Class:						");
+
+	switch (e_ident[EI_CLASS])
+	{
+	case ELFCLASSNONE:
+		printf("none\n");
+		break;
+	case ELFCLASS32:
+		printf("ELF32\n");
+		break;
+	case ELFCLASS64:
+		printf("ELF64\n");
+		break;
+	default:
+			printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+	}
+}
