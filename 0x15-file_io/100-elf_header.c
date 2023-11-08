8cb9053b054795019eb549f43056d2faeb18a939
Emmiechege <emilywanjachege@gmail.com>
2023-11-08 12:23:00 -0800

    A program that displays the information contained in the ELF header at the head of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -107,4 +107,10 @@ void print_data(unsigned char *e_ident)
 		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
 	}
 }
-
+/**
+ * print_version - Prints the version of an ELF header
+ * @e_ident: A pointer to an array containing the ELF version
+ */
+void print_version(unsigned char *e_ident)
+{
+	printf(" Version			%d",
